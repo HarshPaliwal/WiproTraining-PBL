e654a745616cf93a1a842a5f4db7030f1022564a
Harsh Paliwal <51661981+HarshPaliwal@users.noreply.github.com>
2020-06-26 03:26:27 +0530

    Create Solution1.java

---
+++ OOPS/Classes And Objects/Solution1.java
@@ -0,0 +1,22 @@
+public class Solution1{
+	public static void main(String[] args){
+		int width=Integer.parseInt(args[0]);
+		int height=Integer.parseInt(args[1]);
+		int depth=Integer.parseInt(args[2]);
+		Box b=new Box(width,height,depth);
+		System.out.println(b.volume());
+	}
+}
+class Box{
+	int width;
+	int height;
+	int depth;
+	Box(int width,int height,int depth){
+		this.width=width;
+		this.height=height;
+		this.depth=depth;
+	}
+	int volume(){
+		return width*height*depth;
+	}
+}
