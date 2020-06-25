7e074de172a2845f39d570065407034d312e3ce3
Harsh Paliwal <51661981+HarshPaliwal@users.noreply.github.com>
2020-06-26 03:20:43 +0530

    Create Solution1A.java

---
+++ Java Fundamentals/Flow Control/Solution1A.java
@@ -0,0 +1,11 @@
+class Solution1A{
+	public static void main(String[] args){
+		int num=Integer.parseInt(args[0]);
+		if(num>0)
+			System.out.println("Positive");
+		else if(num<0)
+			System.out.println("Negative");
+		else
+			System.out.println("Zero");
+	}
+}
